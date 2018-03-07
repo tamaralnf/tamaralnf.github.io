724765fab86812fda12710f9ed2dea2f97cfca25
zifan feng <zifanfeng@wirelessprvnat-172-16-137-30.near.illinois.edu>
2018-03-06 20:26:32 -0600

    add more scene

--- horizontal_scroll_back1.js
+++ horizontal_scroll_back1.js
@@ -9,7 +9,7 @@ var controller = new ScrollMagic.Controller();
 		// define movement of panels
 		var wipeAnimation = new TimelineMax()
 			.to("#slideContainer", 1,   {x: "-75%"}, 0)
-			
+			.to(".tree-img", 1, {x:"-100%"}, 0)
 			//.to('#water-drop', 1, {x:"135%"}, 0)
 			// .set("#water-drop", 1, {z-index: 2},0)
 
@@ -27,22 +27,20 @@ var controller = new ScrollMagic.Controller();
 		new ScrollMagic.Scene({
 				triggerElement: "#pinContainer",
 				triggerHook: "onLeave",
-				duration: "100%"
+				duration: "40%"
 			})
 			.setTween(TweenMax.to(".grass-img", 1,   {x: "-100%"}, 0))
 			.addIndicators()
 			.addTo(controller);
 		var waterTL = new TimelineMax()
-			.to("#water-drop",  1, {y:"-100%", ease:Power0.easeNone})
+			.to("#water-drop",  1, {y:"-50%", ease:Power0.easeNone})
+			.to("#water-drop", 1, {y:"-30%", ease:Power0.easeNone})
 			.to("#water-drop", 1, {y:"-50%", ease:Power0.easeNone})
-			.to("#water-drop", 1, {y:"-100%", ease:Power0.easeNone})
 			.to("#water-drop", 1, {y:"0%", ease:Power0.easeNone});
 
-
-
 		new ScrollMagic.Scene({
-			offset: 400,
-			duration: "100%"
+			offset: 500,
+			duration: "30%"
 			// triggerHook: "40"
 		})
 		.setTween(waterTL)
@@ -52,34 +50,76 @@ var controller = new ScrollMagic.Controller();
 		.addTo(controller);
 
 
-	  	var images = [
-	  		"waterdrop.png",
-	  		"1.png",
-	  		"2.jpg",
-	  		"3.jpg",
+	  	// var images = [
+	  	// 	// "waterdrop.png",
+	  	// 	"1.png",
+	  	// 	"2.jpg",
+	  	// 	"3.jpg",
 	  		
-	  	];
-  		var img = {curImg: 0};
+	  	// ];
+  		// var img = {curImg: 0};
 
-  		var change_waterdrop = TweenMax.to(img, .5, {
-  			curImg:images.length-1,
-  			roundProps: "curImg",
-  			repeat:3,
-  			immediateRender: true,
-  			ease: Linear.easeNone,
-  			onUpdate: function(){
-  				document.getElementById("water-drop").style.content="url("+images[img.curImg]+")";
-  				document.getElementById("water-drop").style.width="30%";
-  				}
-  		}
-  		);
+  		// var change_waterdrop = TweenMax.to(img, .5, {
+  		// 	curImg:images.length-1,
+  		// 	roundProps: "curImg",
+  		// 	repeat:3,
+  		// 	immediateRender: true,
+  		// 	ease: Linear.easeNone,
+  		// 	onUpdate: function(){
+  		// 		document.getElementById("water-drop").style.content="url("+images[img.curImg]+")";
+  		// 		document.getElementById("water-drop").style.width="30%";
+  		// 		}
+  		// }
+  		// );
+
+	  	// new ScrollMagic.Scene({
+	  	// 	offset: 800,
+	  	// 	duration:400
+	  	// })
+	  	// .setTween(change_waterdrop)
+	  	// .addIndicators({name: "watershape"})
+	  	// .addTo(controller);
+
+	  	// make the intro un transparent
+	  	var sceneTwo = new TimelineMax()
+	  		.to(".two", 1, {y:"-10%"})
+	  		.to("#intro2", 2, {opacity:"1"})
+	  		.to(".two", 1, {y:"0%"})
+	  		// .pin("#intro2")
+
 
 	  	new ScrollMagic.Scene({
-	  		offset: 800,
-	  		duration:400
+	  		offset: "150%",
+	  		duration: "40%"
 	  	})
-	  	.setTween(change_waterdrop)
-	  	.addIndicators({name: "watershape"})
+	  	.setTween(sceneTwo)
+	  	.addIndicators({name: "scene 2"})
+	  	.addTo(controller);
+
+	  	// make the intro un transparent
+	  	var sceneThree = new TimelineMax()
+	  		.to(".three", 1, {y:"-20%"})
+	  		.to("#intro3", 2, {opacity:"1"})
+	  		.to("#water-drop", 2, {y:"-100%"})
+
+	  	new ScrollMagic.Scene({
+	  		offset: "750%",
+	  		duration: "40%"
+	  	})
+	  	.setTween(sceneThree)
+	  	.addIndicators({name: "scene 3"})
+	  	.addTo(controller);
+
+
+	  	var sceneFour = new TimelineMax()
+	  		.to("#water-drop", 2, {y:"-150%"})
+
+	  	new ScrollMagic.Scene({
+	  		offset: "1400%",
+	  		duration: "10%"
+	  	})
+	  	.setTween(sceneFour)
+	  	.addIndicators({name: "scene 4"})
 	  	.addTo(controller);
   	
 // trigger a TweenMax.to tween
